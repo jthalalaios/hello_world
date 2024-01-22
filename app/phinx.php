e988bbbf37c171960746d343e9c718e2a5f0fc08
John Thalalaios <48445852+jthalalaios@users.noreply.github.com>
2024-01-22 20:22:47 +0200

    Add files via upload

---
+++ app/phinx.php
@@ -0,0 +1,41 @@
+<?php
+
+return
+    [
+        'paths' => [
+            'migrations' => '%%PHINX_CONFIG_DIR%%/Migrations',
+            'seeds' => '%%PHINX_CONFIG_DIR%%/Seeds'
+        ],
+        'environments' => [
+            'default_migration_table' => 'phinxlog',
+            'default_environment' => 'development',
+            'production' => [
+                'adapter' => 'mysql',
+                'host' => 'mysql-hello_world:3306',
+                'name' => 'hello_world_db',
+                'user' => 'root',
+                'pass' => 'hello_world',
+                'port' => '5346',
+                'charset' => 'utf8',
+            ],
+            'development' => [
+                'adapter' => 'mysql',
+                'host' => 'mysql-hello_world:3306',
+                'name' => 'hello_world_db',
+                'user' => 'root',
+                'pass' => 'hello_world',
+                'port' => '5346',
+                'charset' => 'utf8',
+            ],
+            'testing' => [
+                'adapter' => 'mysql',
+                'host' => 'localhost',
+                'name' => 'testing_db',
+                'user' => 'root',
+                'pass' => '',
+                'port' => '3306',
+                'charset' => 'utf8',
+            ]
+        ],
+        'version_order' => 'creation'
+    ];
